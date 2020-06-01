5954e86b34d99fbdefaf00015ac23adf08b91f96
Mike Buyco <mbuyco@protonmail.com>
2020-06-02 01:24:28 +0800

    Initial commit

---
+++ src/modules/timer/countdown-timer.js
@@ -0,0 +1,69 @@
+const moment = require('moment');
+
+const DEFAULT_FLOW_DURATION_MINUTES = 5; 
+const DEFAULT_BREAK_DURATION_MINUTES = 5;
+const TIMER_MODES = {
+  BREAK: 'break',
+  FLOW: 'flow',
+};
+
+const defaultTimerValue = moment('00:00', 'mm:ss');
+const flowTimerStart = moment(defaultTimerValue).add(DEFAULT_FLOW_DURATION_MINUTES, 'minutes');
+const breakTimerStart = moment(defaultTimerValue).add(DEFAULT_BREAK_DURATION_MINUTES, 'minutes');
+
+let countdownTime = moment(flowTimerStart);
+let timerId;
+
+const countdownTimer = {
+  getCurrentTime() {
+    return this.getFormattedTime(countdownTime);
+  },
+
+  getFormattedTime(time, format = 'mm:ss') {
+    return moment(time, 'mm:ss').format(format)
+  },
+
+  getStartTime(mode = TIMER_MODES.FLOW) {
+    return this.getFormattedTime(mode === TIMER_MODES.FLOW ? flowTimerStart : breakTimerStart);
+  },
+
+  onStart() {},
+  onStop() {},
+
+  resetTimer(mode = TIMER_MODES.FLOW) {
+    countdownTime = moment((mode === TIMER_MODES.FLOW) ? flowTimerStart : breakTimerStart);
+  },
+
+  start(callback, mode = TIMER_MODES.FLOW, interval = 1) {
+    console.log(mode);
+    this.onStart(mode);
+    this.tick(callback, mode, interval);
+  },
+
+  stop(callback, mode = TIMER_MODES.FLOW) {
+    this.onStop(mode);
+    this.resetTimer(mode);
+    callback(this.getStartTime(mode));
+    clearTimeout(timerId);
+  },
+
+  tick(callback, mode, interval) {
+    callback(this.getFormattedTime(countdownTime), mode);
+
+    timerId = setTimeout(() => {
+      const timeDiffInSeconds = countdownTime.diff(defaultTimerValue, 'seconds');
+
+      if (timeDiffInSeconds === 0) {
+        this.stop(callback, mode === TIMER_MODES.FLOW ? TIMER_MODES.BREAK : TIMER_MODES.FLOW);
+        return;
+      }
+
+      countdownTime = countdownTime.subtract(1, 'seconds');
+      this.tick(callback, mode, interval);
+    }, interval * 1000);
+  },
+
+  TIMER_MODES,
+};
+
+module.exports = countdownTimer;
---
+++ src/render.js
@@ -0,0 +1,47 @@
+const countdownTimer = require('./modules/timer/countdown-timer');
+const { TIMER_MODES } = countdownTimer;
+
+const timerBgColor = {
+  break: '#ff5050',
+  start: '#95e895',
+};
+
+// default timer mode
+let currentMode = TIMER_MODES.FLOW;
+
+// style functions
+const getBgColor = (mode) => mode === TIMER_MODES.FLOW ? timerBgColor.start : timerBgColor.break;
+const setBgColor = (mode) => {
+  document.body.style['background'] = getBgColor(mode);
+};
+const setTime = (time) => {
+  document.getElementById('timer').innerText = time;
+};
+
+// timer start event binding
+countdownTimer.onStart = (mode) => {
+  setBgColor(mode);
+};
+
+// timer stop event binding
+countdownTimer.onStop = (mode) => {
+  currentMode = mode;
+  setBgColor(currentMode);
+};
+
+// initialize start button click event
+document.getElementById('start').onclick = () => {
+  countdownTimer.start((time) => {
+    setTime(time);
+  }, currentMode);
+};
+
+// stop button click event
+document.getElementById('stop').onclick = () => {
+  countdownTimer.stop((time) => {
+    setTime(time);
+  }, TIMER_MODES.FLOW);
+};
+
+// initialize timer
+document.getElementById('timer').innerHTML = countdownTimer.getStartTime();
